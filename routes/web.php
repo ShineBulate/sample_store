0292f1ad7101eea8558a47da5dfb3e5dd012a78e
admin <admin@MacBook-Air-admin.local>
2025-06-16 09:37:50 +0800

    Init routes Product

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/products');
 });
+Route::get('/products', 'App\Http\Controllers\Product\IndexController')->name('products.main');;
+Route::get('/products/create', 'App\Http\Controllers\Product\CreateController')->name('products.create');
+Route::post('/products/store', 'App\Http\Controllers\Product\StoreController')->name('products.store');;
+Route::get('/products/{product}', 'App\Http\Controllers\Product\ShowController')->name('products.show');
+Route::get('/products/{product}/edit', 'App\Http\Controllers\Product\EditController')->name('products.edit');
+Route::post('/products/{product}', 'App\Http\Controllers\Product\UpdateController')->name('products.update');;
+Route::get('/products/{product}', 'App\Http\Controllers\Product\DestroyController')->name('products.delete');
+
+
